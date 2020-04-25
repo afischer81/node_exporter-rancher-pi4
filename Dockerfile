13f2d80c2e9aef73f1fc2f9115b126f22f03888c
Alexander Fischer <47056554+afischer81@users.noreply.github.com>
2020-04-25 13:36:10 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM alpine AS builder
+
+LABEL maintainer "Alexander Fischer <alexander.fischer.ac@googlemail.com>"
+
+ARG ARCH=armv7
+ARG VERSION=1.0.0-rc.0
+
+RUN apk --no-cache add wget ca-certificates \
+    && mkdir -p /tmp/install \
+    && wget -O /tmp/install/node_exporter.tar.gz https://github.com/prometheus/node_exporter/releases/download/v$VERSION/node_exporter-$VERSION.linux-$ARCH.tar.gz \
+    && cd /tmp/install \
+    && tar --strip-components=1 -xzf node_exporter.tar.gz
+
+FROM scratch
+
+COPY --from=builder /tmp/install/node_exporter /bin/
+
+EXPOSE 9100
+ENTRYPOINT [ "/bin/node_exporter" ]
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IMAGE=node_exporter/aarch64
+
+function do_build {
+    docker build -t ${IMAGE} .
+}
+
+function do_run {
+    docker run -d -p 9100:9100 \
+        -v "/proc:/host/proc:ro" \
+        -v "/sys:/host/sys:ro" \
+        -v "/:/rootfs:ro" \
+        -v "/mnt/docker:/dockerfs:ro" \
+        -v "/mnt/opt:/optfs:ro" \
+        --net="host" \
+        --name="node_exporter" \
+        --restart unless-stopped \
+        ${IMAGE} \
+        --path.procfs /host/proc \
+        --path.sysfs /host/sys \
+        --collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)" 
+}
+
+do_$1
